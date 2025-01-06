6d93db76373df42da5d49ec3f496380ebec174ad
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-06 13:54:47 +0530

    Practical - 1

---
+++ P1/PA1Q7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.P1
+{
+    internal class PA1Q7
+    {
+        static void Main(string[] args)
+        {
+            int second = 0;
+            Console.Write("Enter Seconds : ");
+            second = Int32.Parse(Console.ReadLine());
+        
+            Console.WriteLine("Seconds to Minutes : " + second / 60 + " minutes");
+            Console.WriteLine("Seconds to Hours : " + second / 3600 + " hours");
+        }
+    }
+}
