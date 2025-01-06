ff11e7b864c6eeb5fbe80218485e36379138030c
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-06 13:54:47 +0530

    Practical - 1

---
+++ P1/PA1Q8.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.P1
+{
+    internal class PA1Q8_1
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.Write("Enter a number : ");
+            num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Table of " + num + " : ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num + " x " + i + " = " + num * i);
+            }
+        }
+    }
+}
