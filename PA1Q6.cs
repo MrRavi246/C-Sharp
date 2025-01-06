08285e54f61851d95215b49ef53a1e603645ed4b
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-06 14:01:41 +0530

    Revert "Practical - 1"
    This reverts commit ff11e7b864c6eeb5fbe80218485e36379138030c.

---
+++ PA1Q6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class PA1Q6
+    {
+        static void Main(string[] args)
+        {
+            double mm = 0, cm = 0, m= 0,km = 0;
+
+            Console.Write("Enter mm : ");
+            r = Double.Parse(Console.ReadLine());
+
+            area = pi * r * r;
+
+            Console.WriteLine("Area of Circle is : " + area);
+        }
+    }
+}
