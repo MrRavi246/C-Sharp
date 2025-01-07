9e432e026e71f437e1c364e0671126963de45650
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-07 10:52:07 +0530

    07-01-2025

---
+++ OOPs/book1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.OOPs
+{
+    public class book
+    {
+        double price;
+        int pages;
+        string colour;
+
+        public void getData()
+        {
+            price = 70;
+            pages = 120;
+            colour = "red";
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("The price of book is : " + price);
+            Console.WriteLine("Book pages are : " + pages);
+            Console.WriteLine("Colour of book : " + colour);
+        }
+    }
+    class book1
+    {
+        public static void Main(string[] args)
+        {
+            book b1 = new book();
+            b1.getData();
+            b1.showData();
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ P1/PA1Q8.1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.P1
+{
+    internal class PA1Q8
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.Write("Enter number : ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Multiplication Table of " + num + " is : ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(num + " x " + i + " = " + num * i);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
