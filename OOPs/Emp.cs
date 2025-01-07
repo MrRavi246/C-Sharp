d384b16841948381c22222b6037cd86ab10ffd9a
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-07 11:36:19 +0530

    Add Employees class and Emp main program
    Introduced a new C# class `Employees` in the `C_Sharp.OOPs` namespace with fields for `id`, `name`, `age`, and `salary`, and methods `getData` and `showData` for input and display of employee details. Added an internal class `Emp` with a `Main` method to create instances of `Employees`, input data, and display it. Updated `Emp.cs` with necessary `using` directives. Updated `emp.exe` to reflect new changes. Added an XML manifest file specifying assembly identity and security settings.

---
+++ OOPs/Emp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.OOPs
+{
+    public class Employees
+    {
+        int id;
+        string name;
+
+        int age;
+        int salary;
+
+        public void getData()
+        {
+            Console.Write("Enter Employee ID : ");
+            id = Int16.Parse(Console.ReadLine());
+
+            Console.Write("Enter Employee Name : ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Employee Age : ");
+            age = Int16.Parse(Console.ReadLine());
+
+            Console.Write("Enter Employee Salary : ");
+            salary = Int32.Parse(Console.ReadLine());
+        }
+
+        public void showData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------Employee Details-------------");
+
+            Console.WriteLine("Employee Id is : " + id);
+            Console.WriteLine("Employee Name : " + name);
+            Console.WriteLine("Employee Age : " + age);
+            Console.WriteLine("Employee Salary : " + salary);
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine();
+
+        }
+    }
+    internal class Emp
+    {
+        public static void Main(string[] args)
+        {
+            Employees emp1 = new Employees();
+
+            emp1.getData();
+            emp1.showData();
+
+            Employees emp2 = new Employees();
+
+            emp2.getData();
+            emp2.showData();
+
+            Employees emp3 = new Employees();
+
+            emp3.getData();
+            emp3.showData();
+        }
+    }
+}
