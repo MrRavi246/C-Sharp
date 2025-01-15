4843117947326a47f9b701517141726fcf073f74
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-15 08:35:11 +0530

    Add Phyramid class to print pyramid pattern in C#
    Introduced a new C# class named `Phyramid` within the `C_Sharp.Loop` namespace. This class includes a `Main` method that prints a pyramid pattern of asterisks using nested `for` loops. Added `using` directives for common namespaces. Included `Console.ReadLine()` to keep the console open. Updated `Phyramid.exe` with the compiled output. Added an XML manifest file to specify assembly identity and execution level.

---
+++ Loop/Phyramid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Loop
+{
+    internal class Phyramid
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 0; i <= 5; i++)
+            {
+                Console.Write("*");
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
