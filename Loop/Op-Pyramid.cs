42f0278664528d66d4fc7ba2a6b1f8056ea6af1d
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-15 09:25:54 +0530

    Add pyramid pattern program and related files
    Added a new C# source file `Op-Pyramid.cs` that includes a program to print a pyramid pattern using nested loops within the `C_Sharp.Loop` namespace. The program's `Main` method prints a pyramid of asterisks.
    
    Added the compiled executable file `Op-Pyramid.exe`.
    
    Added an XML manifest file specifying assembly identity, version, and requested execution level.

---
+++ Loop/Op-Pyramid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Loop
+{
+    internal class Op_Pyramid
+    {
+        public static void Main(string[] arg)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= 5 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
