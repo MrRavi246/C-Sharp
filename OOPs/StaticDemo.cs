6901f106ef633db0c0059fb099baa41516ed15e5
MrRavi246 <rsardhara451@rku.ac.in>
2025-01-28 10:22:26 +0530

    Add StaticDemo program with Author class and manifest
    Introduce StaticDemo.cs with a static Author class containing
    static fields and a Display method. Add StaticDemo class with
    Main method to call Author.Display() and wait for a key press.
    Update StaticDemo.exe to reflect new code. Include XML manifest
    file to specify assembly identity and execution level.

---
+++ OOPs/StaticDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.OOPs
+{
+    public static class Author
+    {
+        public static string name = "XYZ";
+        public static string title = "C# programming";
+
+        public static void Display()
+        {
+            Console.WriteLine("Author Name : " + name);
+            Console.WriteLine("Title : " + title);
+        }
+    }
+    public class StaticDemo
+    {
+        public static void Main(string[] args)
+        {
+            Author.Display();
+            Console.ReadKey();
+        }
+    }
+
+
+}
