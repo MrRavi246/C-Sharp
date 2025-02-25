ef281762bc1d105bea3268360e1f443faa585563
MrRavi246 <rsardhara451@rku.ac.in>
2025-02-25 11:31:28 +0530

    Add C# program demonstrating interfaces and multiple inheritance
    Introduce InterfaceDemo.cs with interfaces and class implementation.
    - Add using directives and define namespace C_Sharp.OOPs.
    - Create interfaces InShape and Color.
    - Implement InRectangle class inheriting from both interfaces.
    - Add InterfaceDemo class with Main method to demonstrate usage.
    Update InterfaceDemo.exe with compiled executable.
    Add XML manifest file with assembly identity and trust info.

---
+++ OOPs/InterfaceDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.OOPs
+{
+
+    public interface InShape
+    {
+        double getArea(double l , double h);
+    }
+    public interface Color
+    {
+        void getColor(string c);
+    }
+    public class InRectangle : InShape, Color       //Multiple Inheritance
+    {
+        public double getArea(double l, double h)
+        {
+            return l * h;
+        }
+
+        public void getColor(string c)
+        {
+            Console.WriteLine("Color: "+c);
+        }
+    }
+
+ 
+internal class InterfaceDemo
+    {
+        static void Main(string[] args)
+        {
+            InRectangle r = new InRectangle();
+
+
+            Console.WriteLine("Area of Rectangle : " + r.getArea(10, 15));
+            r.getColor("RED");
+        }
+    }
+}
